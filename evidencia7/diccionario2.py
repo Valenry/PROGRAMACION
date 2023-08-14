e5c4a8aad0fdc27156a0030a291f9769290ac3a4
Valenry <131418384+Valenry@users.noreply.github.com>
2023-08-14 17:04:56 -0300

    diccionario2.py

--- evidencia7/diccionario2.py
+++ evidencia7/diccionario2.py
@@ -1,27 +1,16 @@
-# Diccionario con claves autogeneradas y valores de Nª de Tel.
+import random
+
+# Crear diccionario
 numeros_telefono = {}
 contador = 1
 
-while True:
-    telefono = input(f"Ingrese el número de teléfono para Persona{contador} (o 'salir' para finalizar): ")
-    
-    if telefono.lower() == 'salir':
-        break
-    
-    numeros_telefono[contador] = telefono
+while contador <= 5:  # Puedes cambiar este valor para tener más claves
+    telefono = f"{random.randint(100, 999)}-{random.randint(100, 999)}-{random.randint(1000, 9999)}"
+    numeros_telefono[f"Persona{contador}"] = telefono
     contador += 1
 
-# Mostrarlo 
+# Mostrar el diccionario 
 print("Diccionario de números de teléfono:")
 for clave, telefono in numeros_telefono.items():
     print(f"Clave: {clave}, Teléfono: {telefono}")
 
-
-# Método de prueba eficiente :(
-diccionario_prueba = {1: "123-456-7890", 2: "987-654-3210", 3: "555-555-5555"}
-print("\nMétodo de prueba eficiente:")
-for clave, telefono in diccionario_prueba.items():
-    print(f"Clave: {clave}, Teléfono: {telefono}")
-
-
-# DIO!! NO TOCAR MAS!
